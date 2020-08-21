7f952e4eb9c8e3ac09331c5208c2f40961648de2
Sam <sphilipsminecraft@gmail.com>
2020-08-21 16:53:54 +0100

    Initial Commit

---
+++ ShopSystem/BuySys.py
@@ -0,0 +1,60 @@
+# Some Things we'll need
+#StockLibrary = [{'p3': 4, 'p5': 0, 'p7': 0}, {'16GB': 0, '32GB': 7}, {'1TB': 9, '2TB': 12}, {'16 Inch': 6, '23 Inch': 9}, {'Mini': 17, 'Midi': 12}, {'2P': 23, '4P': 18}]
+
+ItemLibrary = [{"p3-3410i":100,"p5-3420i":120,"p7-3430i":200}, # Processors
+    {"16 GB":75,"32 GB":150},                # RAM
+    {"1 TB":50,"2 TB": 100},                 # Storage
+    {"19 Inch":65,"23 Inch":120},            # Screen
+    {"Mini Tower":40,"Midi Tower":70},       # Case
+    {"2 Ports":10,"4 Ports":20}              # USB Ports
+]
+ComponentType = ["Processor","RAM Size","Storage Option","Screen Size","Case Size","USB Port Amount"]
+
+def BuySystemV1(StockLibrary): # This doesn't really work yet but whatever
+    """Standard buy screen. Requires a list of stock."""
+    print("\nWelcome to the PC building screen!\nLets begin by selecting the component you'd like from the list below:")
+    SelectedComponents = []
+    for Type in range(6):
+        print(f"COMPONENT: {ComponentType[Type]}")
+        ItemKeys = ItemLibrary[Type].keys()
+        StockKeys = StockLibrary[Type].keys()
+        Numb_Items = 1
+        Available_Items = []
+        for ItemKey,StockKey in zip(ItemKeys,StockKeys):
+            Availability = "" 
+            if not StockLibrary[Type][StockKey]:
+                Availability = "[NOT IN STOCK]"
+                Available_Items.append(None)
+            else:
+                Available_Items.append(ItemLibrary[Type][ItemKey])
+            print(f"{Numb_Items}: {ItemKey} -> ${ItemLibrary[Type][ItemKey]} {Availability}")
+            Numb_Items += 1
+        while True:
+            try:
+                ItemSelection = int(input("\nPlease make your selection.\nINPUT: "))
+                if (ItemSelection > 0 and ItemSelection <= len(ItemKeys)) and Available_Items[ItemSelection-1] is not None:
+                    SelectedComponents.append(([q for q in StockKeys][ItemSelection-1])) # Do something to do with tables [q for q in StockKeys]
+                    break
+                else:
+                    raise ValueError
+            except (ValueError,IndexError):
+                print("[ERROR] That is not a selection!")
+    print(SelectedComponents)
+    # Final Pricing and stuff
+    print("Total Cost:\n[COMPONENT]{}[COST]".format("-"*11))
+    finalcost = 0
+    for i in enumerate(ItemLibrary):
+        cost = i[1][SelectedComponents[i[0]]] # Looks complex, but it just accesses the tuple and stuff
+        print(SelectedComponents[i[0]]," "*(20-len(SelectedComponents[i[0]])),f" ${cost}")
+        finalcost+=cost
+    finalcost *= 1.2 # VAT
+    visual_Cost = finalcost
+    if finalcost.is_integer():
+        visual_Cost = str(int(visual_Cost))
+        visual_Cost+=".00"
+    else:
+        visual_Cost = str(visual_Cost)
+        visual_Cost+="0"
+    print(" "*8,f"GRAND TOTAL:  ${visual_Cost} (with 20% VAT)")
+    
+    return finalcost,SelectedComponents
---
+++ ShopSystem/shopSystem.py
@@ -0,0 +1,90 @@
+# File name: shopSystem.py
+# Aim: Create a basic computer shop system that estimates the cost of items and does some other cool stuff, I guess.
+import csv
+import time
+from BuySys import BuySystemV1
+
+def StockRefresh():
+    """Refreshes the stock levels of all items available. No arguments."""
+    with open("shopStock.csv",newline='') as Stock:
+        Stock_List = list(csv.reader(Stock))
+        Stock.close
+        for item in Stock_List:
+            try:
+                while True:
+                    item.remove('')
+            except ValueError:
+                continue
+    # Converting List into a Dict
+    StockLibrary = []
+    for Component in Stock_List:
+        TempDict = {}
+        for item in Component:
+            split = item.split(" : ")
+            TempDict[split[0]] = int(split[1])
+        StockLibrary.append(TempDict)
+    return StockLibrary
+
+StockLibrary = StockRefresh() # This is run to make sure any functions or modules always have an inital Stock level to go off of.
+
+def StockUpdate(list,SaveOnly=False):
+    """Updates the stock of items. Requires a list of items to update to subtract from each component by 1. It also saves back to the main file. """
+    if list: # Checks to see if list has items. [] = false, while [1,2] = true!
+        if not SaveOnly:
+            for component in StockLibrary:
+                for item in list:
+                    if component.get(item):
+                        component.update({item:component.get(item)-1})
+                    
+        # Saving to CSV file.           
+        with open("shopStock.csv","w+",newline='') as document:
+            Writer = csv.writer(document,delimiter=',')
+            for part in StockLibrary:
+                listAp = []
+                for key in part.keys():
+                    line = "{} : {}".format(key,part[key])
+                    listAp.append(line)
+                Writer.writerow(listAp)
+            document.close()
+    else:
+        print("[STOCK MODULE ERROR] No items listed to update!")
+
+# StockUpdate([{'p3': 4, 'p5': 8, 'p7': 4}, {'16GB': 10, '32GB': 7}, {'1TB': 9, '2TB': 12}, {'16 Inch': 6, '23 Inch': 9}, {'Mini': 17, 'Midi': 12}, {'2P': 23, '4P': 18}],SaveOnly=True)
+
+# This library is the pricing list for all items
+ItemLibrary = [{"p3-3410i":100,"p5-3420i":120,"p7-3430i":200}, # Processors
+    {"16 GB":75,"32 GB":150},                # RAM
+    {"1 TB":50,"2 TB": 100},                 # Storage
+    {"19 Inch":65,"23 Inch":120},            # Screen
+    {"Mini Tower":40,"Midi Tower":70},       # Case
+    {"2 Ports":10,"4 Ports":20}              # USB Ports
+]
+
+def shopUI():
+    """Its the shop UI, duh."""
+    #Stock = StockRefresh()
+    print("[COMPUTER SHOP]\nWelcome To My Computer Store! Please select one of the options below:\n1 - Buying a PC\n2 - Current Stock\n3 - Admin Console")
+    def BuyPC(): # Buying PC
+        final_cost,User_Components = BuySystemV1(StockRefresh())
+        print(final_cost,User_Components)
+        
+    def ShowStock():# Show current stock
+        ComponentType = ["Processor","RAM Size","Storage","Screen Size","Case Size","USB Ports"]
+        for i in range(len(ComponentType)-1):
+            print("\n"+ComponentType[i])
+            for Name,Amount in zip(StockRefresh()[i].keys(),StockRefresh()[i].values()):
+                print(f"{Name}: {Amount} in storage.\n")
+
+    def AdminPanel(): # Admin panel: Add stock, check recent orders and so on
+        pass
+
+    Switch_Case = {1:BuyPC,2:ShowStock,3:AdminPanel}
+    while True:
+        try:
+            userSelection = int(input("Make your pick: "))
+            Switch_Case[int(userSelection)]()
+            break
+        except (KeyError,ValueError):
+            print("That is not a category, pick again!\n")
+while True:
+    shopUI()
