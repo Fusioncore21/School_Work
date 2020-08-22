0232a6c019a9fba56462a5958965b1106d04c8b6
Sam <sphilipsminecraft@gmail.com>
2020-08-22 10:04:23 +0100

    Began making a product_ID generator

--- ShopSystem/NamingSystem.py
+++ ShopSystem/NamingSystem.py
@@ -1,13 +1,16 @@
 from string import ascii_uppercase as upper
 upper = [a for a in upper]
 
-def Convert_Numb_To_Ref(Numb):
-    Letter_2 = upper[Numb//9999]
-    Counter = 0
-    if Letter_2=="F":
-        Counter += 1
-    Letter_1 = upper[Counter]
-    print(f"#{Letter_1}{Letter_2}")
+def Convert_Numb_To_Ref(Order_Number):
+    """Takes an Order_Number (int), and converts it into a unique Order_ID"""
+    Number = str((Order_Number%9999)+1) # Makes it so it never is bigger than 4 digits
+    Number = ("0"*(4-len(str(Number))))+Number # If number < 1000, adds on 0s to the beginning, so 1 -> 0001
+    try:
+        Letter = upper[Order_Number//9999] # Divides and floors
+    except IndexError:
+        print("Number too large! Cannot generate Product_ID")
+        return False
+    return f"#{Letter}{Number}"
 while True:
-    a = int(input("Input: "))
-    Convert_Numb_To_Ref(a)
+    Product_UD = Convert_Numb_To_Ref(int(input("Number please: ")))
+    print(Product_UD)
